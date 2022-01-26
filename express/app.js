6218fac62cab2b715d576420792303edb6e0cb47
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2022-01-26 22:56:58 +0530

    Static files

--- express/app.js
+++ express/app.js
@@ -1,7 +1,23 @@
 const express=require("express");
-
+const path=require("path")
 const app=express();
-const port=111;
+const port=333;
+
+//serving static files
+app.use('/static',express.static('static'));
+
+// set the template engine as pug
+
+app.set('view engine','pug');
+
+//set the view directory
+app.set('views',path.join(__dirname,'views'));
+
+//our pug demo end point
+app.get("/demo",(req,res)=>{
+    res.status(200).render('demo', { title: 'Hey', message: 'Hello there!' })
+});
+
 
 app.get("/",(req,res)=>{
     res.status(200).send("This is homepage of my first express app with Harry");
--- tut7/tut7.js
+++ tut7/tut7.js
@@ -2,3 +2,13 @@ console.log("Tutorial 7");
 
 //npm is a packet manager for node modules.
 
+// version: .major.minor.patch
+//remove function.new function.bugs
+
+//nodemon starts server automatically
+
+//global :use from wherever you can
+
+// ^ : exactly the same version
+//~ accepts the new patch
+// >  installs the latest version
