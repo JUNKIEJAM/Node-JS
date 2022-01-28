d24fa0a1162aeb29e83946e4bc4ae8e97074ef14
JUNKIEJAM <66085589+JUNKIEJAM@users.noreply.github.com>
2022-01-28 19:08:19 +0530

    Using raw HTML in Pug template engine

--- express/app.js
+++ express/app.js
@@ -3,6 +3,7 @@ const path=require("path")
 const app=express();
 const port=333;
 
+//EXPRESS SPECIFIC STUFF
 //serving static files
 app.use('/static',express.static('static'));
 
@@ -10,9 +11,11 @@ app.use('/static',express.static('static'));
 
 app.set('view engine','pug');
 
+//PUG Specific Stuf
 //set the view directory
 app.set('views',path.join(__dirname,'views'));
 
+/*
 //our pug demo end point
 app.get("/demo",(req,res)=>{
     res.status(200).render('demo', { title: 'Hey', message: 'Hello there!' })
@@ -33,10 +36,20 @@ app.post("/about",(req,res)=>{
 
 app.get("/this",(req,res)=>{
     res.status(404).send("This page is not found");
-});
+}); */
+
+//END POINTS
+
+app.get('/',(req,res)=>{
+     
+    const con="This is the best content on website";
+    const params={'title':'This is the best website',"content":con};
+    res.status(200).render('index.pug',params)    // render for template and not send
+})
 
 
 
+//Start the server
 app.listen(port,()=>{
     console.log(`This application started successfully on port ${port}`);
 });
